a0ee7a037702a06c9f6e73c9e4fff9873d3fda1d
if0ne <pavel.agafonov.al@gmail.com>
2024-07-23 17:34:51 +0300

    Add: vk get user profile route

--- orkestra-auth-system/src/shared/integrations/vk/controller.rs
+++ orkestra-auth-system/src/shared/integrations/vk/controller.rs
@@ -1,17 +1,27 @@
 use std::net::SocketAddr;
 
-use axum::{extract::ConnectInfo, response::IntoResponse, Extension, Json};
+use axum::{
+    extract::{ConnectInfo, Query},
+    response::IntoResponse,
+    Extension,
+};
 
 use tracing::{error, info, info_span, Instrument};
 
-use crate::shared::utils::{bad_request_json, just_ok};
+use crate::shared::{
+    integrations::vk::dto::UserProfileResponse,
+    utils::{bad_request_json, just_ok, ok},
+};
 
-use super::{api::VkService, dto::VkAuthData};
+use super::{
+    api::VkService,
+    dto::{UserProfileData, VkAuthData},
+};
 
 pub async fn auth(
     Extension(context): Extension<VkService>,
     ConnectInfo(ip): ConnectInfo<SocketAddr>,
-    Json(request): Json<VkAuthData>,
+    Query(request): Query<VkAuthData>,
 ) -> impl IntoResponse {
     let span = info_span!("vk_auth");
     let _guard = span.enter();
@@ -48,6 +58,40 @@ pub async fn auth(
 
             bad_request_json(serde_json::json!({
                 "error": err.to_string()
+            }))
+        }
+    }
+}
+
+pub async fn get_user_profile(
+    Extension(context): Extension<VkService>,
+    Query(request): Query<UserProfileData>,
+) -> impl IntoResponse {
+    let span = info_span!("vk_user_profile");
+    let _guard = span.enter();
+
+    info!(event = "Request user profile in VK", username = request.uid);
+
+    let result = context
+        .get_user_profile(&request.uid)
+        .in_current_span()
+        .await;
+
+    match result {
+        Ok(data) => {
+            info!(event = "Successfully got user profile");
+
+            let response = UserProfileResponse {
+                nickname: data.nick,
+            };
+
+            ok(response)
+        }
+        Err(err) => {
+            error!(event = err.errmsg);
+
+            bad_request_json(serde_json::json!({
+                "error": err.errmsg
             }))
         }
     }
